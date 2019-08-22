e8d18e68672fa4103daf3a698965132388b76d09
Dylan Thomas <dylanpthomas02@gmail.com>
2019-08-22 16:21:42 +1000

    Putter snap to hand working.

--- PuttWorld/Assets/VRGrab.cs
+++ PuttWorld/Assets/VRGrab.cs
@@ -2,53 +2,53 @@
 
 public class VRGrab : MonoBehaviour
 {
+    public GameObject controller;
     public string axisName;
-    GameObject CollidingObject;
-    GameObject grabbedObject;
 
-    public void OnTriggerEnter(Collider other)
+    private float triggerState = 0;
+
+    private bool holdingPutter = false;
+    private GameObject putter = null;
+
+    public Vector3 holdPosition = new Vector3(0, -0.025f, 0.03f);
+    public Vector3 holdRotation = new Vector3(0, 180, 0);
+
+    void Update()
     {
-        if (other.gameObject.GetComponentInParent<VRGrabbable>())
+        triggerState = Input.GetAxis(axisName);
+    }
+
+    void Grab(GameObject obj)
+    {
+        holdingPutter = true;
+        putter = obj;
+
+        putter.transform.parent = transform;
+
+        putter.transform.localPosition = holdPosition;
+        putter.transform.localEulerAngles = holdRotation;
+    }
+
+    void Release(GameObject obj)
+    {
+        holdingPutter = false;
+        putter = null;
+
+        putter.transform.SetParent(null);
+
+        Rigidbody rb = putter.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Putter"))
         {
-            CollidingObject = other.gameObject;
+            if (triggerState > 0.5f && !holdingPutter)
+            {
+                Grab(other.gameObject);
+            }
         }
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        CollidingObject = null;
-    }
-
-    void Update() 
-    {
-        if (Input.GetAxis(axisName) > 0.2f && CollidingObject)
-        {
-            GrabObject();
-        }
-
-        if (Input.GetAxis(axisName) < 0.2f && grabbedObject)
-        {
-            ReleaseObject();
-        }
-    }
-
-    public void GrabObject() 
-    {
-        grabbedObject = CollidingObject;
-        grabbedObject.transform.SetParent(gameObject.transform);
-        VRGrabbable grabbable = grabbedObject.GetComponent<VRGrabbable>() ?? grabbedObject.GetComponentInParent<VRGrabbable>();
-        if (grabbable == null) return;
-
-        grabbedObject.transform.SetPositionAndRotation(grabbable.snapOffset.position,
-            grabbable.snapOffset.rotation);
-        Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
-        rb.isKinematic = true;
-    }
-
-    private void ReleaseObject()
-    {
-        grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
-        grabbedObject.transform.SetParent(null);
-        grabbedObject = null;
     }
 }
