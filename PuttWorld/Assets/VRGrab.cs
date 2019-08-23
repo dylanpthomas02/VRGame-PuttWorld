fb63468e110036024f3b225bfbdc0d56ae309088
Dylan Thomas <dylanpthomas02@gmail.com>
2019-08-23 20:59:32 +1000

    Added release object method

--- PuttWorld/Assets/VRGrab.cs
+++ PuttWorld/Assets/VRGrab.cs
@@ -1,21 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class VRGrab : MonoBehaviour
 {
     public GameObject controller;
     public string axisName;
-
-    private float triggerState = 0;
-
-    private bool holdingPutter = false;
-    private GameObject putter = null;
-
     public Vector3 holdPosition = new Vector3(0, -0.025f, 0.03f);
     public Vector3 holdRotation = new Vector3(0, 180, 0);
+
+    private float triggerState = 0;
+    private bool holdingPutter = false;
+    private GameObject putter = null;
 
     void Update()
     {
         triggerState = Input.GetAxis(axisName);
+
+        if (holdingPutter)
+        {
+            if (triggerState < 0.8f)
+            {
+                Release();
+            }
+        }
+    }
+
+    private void Release()
+    {
+        putter.transform.parent = null;
+        Rigidbody rb = putter.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
     }
 
     void Grab(GameObject obj)
@@ -45,7 +60,7 @@ public class VRGrab : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Putter"))
         {
-            if (triggerState > 0.5f && !holdingPutter)
+            if (triggerState > 0.8f && !holdingPutter)
             {
                 Grab(other.gameObject);
             }
