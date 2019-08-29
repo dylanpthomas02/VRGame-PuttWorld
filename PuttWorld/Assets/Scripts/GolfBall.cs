646e48301ca2efcdc14c1fc2836540805d944c3f
Dylan Thomas <dylanpthomas02@gmail.com>
2019-08-29 18:05:56 +1000

    Lots of environment changes. Added a small audio clip for putting

---
+++ PuttWorld/Assets/Scripts/GolfBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour
+{
+    AudioSource source;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Putter"))
+        {
+            source.PlayOneShot(source.clip);
+        }
+    }
+}
